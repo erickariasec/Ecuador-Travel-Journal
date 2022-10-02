cea2966ff55eaf1a553ab3c3ac186cb0ae1f13ca
erickariasec <erickarias1999@hotmail.com>
2022-10-02 12:54:14 -0500

    Fixing logo image load

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import "./Navbar.css"
+import ecuadorLogo from "/assets/ecuador-logo.png"
 
 export default function Navbar() {
     return (
         <nav className="navbar">
-            <img src="/assets/ecuador-icon.png" alt="Ecuador Icon" className="logo" />
+            <img src={ecuadorLogo} alt="Ecuador Icon" className="logo" />
             <h1 className="title">Ecuador Travel Journal</h1>
         </nav>
     );
